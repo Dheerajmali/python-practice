023866300c14e3ba75fda400db07a01ce22cbe6b
Dheerajmali <114793967+Dheerajmali@users.noreply.github.com>
2022-10-14 09:31:57 +0530

    python

---
+++ list.py
@@ -0,0 +1,15 @@
+#my_list = ['banana ', 'orange' , 'apple' , 'guava']
+#my_list = [400,250,10,20,30]
+#my_list.sort(reverse = true)
+#print(my_list)
+#my_list = ['banana ', 'Orange' , 'Apple' , 'guava']
+#my_list.sort()
+#print(my_list)
+
+list1 = [10,20,30,40]
+list2 = list1.copy()
+print(list2)
+
+list2.pop()
+print(list1)
+print(list2)
---
+++ tuple.py
@@ -0,0 +1,26 @@
+#my_tuple = (10,20,30,40)
+#my_tuple[1] = 100
+
+#print(len(my_tuple))
+#myTuple = (10,)
+#print(type(myTuple))
+
+#myTuple = (10, 'apple','true' , 100)
+#print(mytuple)
+
+#print(my_tuple[2:4])
+#print(my_tuple[:4])
+#print(my_tuple[1:])
+
+#mytuple = (10,20,30,40)
+#mylist = list(mytuple)
+#print(type(mylist))
+#mylist[2] = 100
+#print(mylist)
+#mytuple  = tuple(mylist)
+#print(mytuple)
+
+#mytuple = (10,20,30,40)
+#x += (50,)
+#mytuple = x
+#print(mytuple)
