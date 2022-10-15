0eaa85393963b9d104287f16a67190b451265eb7
Dheerajmali <114793967+Dheerajmali@users.noreply.github.com>
2022-10-15 13:02:42 +0530

    python

---
+++ set.py
@@ -0,0 +1,58 @@
+#myset = {10,20,30,40}
+
+#$print(len(myset))
+
+#mySet = {'csaca', 10, 'world',100, True}
+#print(myset)
+#mySet = set((10,20,30,40))
+#print(mySet)
+
+#access the items in a set
+# = {10,20,30,40}
+#for x in mySet:
+
+   #print(x)
+#(100 in mySet)
+
+#mySet = {'ramesh', 'naresh', 'Aj'}
+
+#mySet.add(10)
+#mySet.add('rajesh')
+#print(mySet)
+
+#set1 = {10,20,30,40}
+#set2 = {50,60}
+#list1 = [50,60]
+#set1.update(list1)
+#print(set1)
+
+#remove items
+#myset = {10,20,30,40}
+
+#myset.remove(20)
+#myset.discard(30)
+#x = myset.pop()
+#print(x)
+#del myset
+#print(myset)
+
+#looping sets
+#myset = {10,20,30,40,50}
+#or x in myset:
+  #  print(x)
+  
+#joining the set
+#set1 = {'lion', 'tiger', 'king'}
+#set2 = {10,20,30,40}
+
+#set3 = set1.union(set2)
+#set1.update(set2)
+#print(set1)
+
+#x = {'lion', 'tiger', 'king'}
+#y = {10,20,30,40}
+
+#z = x.union(y)
+#x.update(y)
+#print(z)
+#print(x)
