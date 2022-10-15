9b63c0d1bf1b51e46611e9bb9b300750bdc695de
Dheerajmali <114793967+Dheerajmali@users.noreply.github.com>
2022-10-15 12:11:32 +0530

    python

---
+++ tuple 1.py
@@ -0,0 +1,48 @@
+#unpacking
+
+#digit = (10,20,30)
+#(dig1, dig2, dig3) = digit
+#print(dig1)
+#print(dig2)
+#print(dig3)
+
+#digit = (10,20,30,40,50)
+
+#(dig1, dig2, *dig3) = digit
+#(dig1, *dig2, dig3) = digit
+
+#print(dig1)
+#print(dig2)
+#print(dig3)
+
+#looping of tuples 
+
+#myTuple = ('milk', 'butter', 'ghee')
+#for x in mytuple:
+ #   print(x)
+
+#loping through index digit
+
+#for i in range(len(myTuple)):
+ #   print(myTuple[i])
+
+#joining tuples
+
+#tuple1 = (10,20,30,40)
+#tuple2 = (50,60,70,80,90)
+
+#tuple3 = tuple1 + tuple2
+#print(tuple3)
+
+#multiplication of 
+#myTuple = ('milk', 'butter', 'ghee')
+
+#multipliedtuple = myTuple *3
+#print(multipliedtuple)
+
+#access the element of tuple
+#mytuple = (10,20,30,40)
+#print(mytuple[-2])
+#print(mytuple[1:])
+#print(mytuple[1:3])
+#print(mytuple[:4])
