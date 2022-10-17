6010b55a223c335ffe9c54a8dd36bb8732b1cbb4
Dheerajmali <114793967+Dheerajmali@users.noreply.github.com>
2022-10-17 13:08:30 +0530

    python

---
+++ strings.py
@@ -0,0 +1,26 @@
+#modify strings
+#x = 'hello world'
+#print(x.upper())
+#print(x.lower())
+
+#remove white spaces
+#x = "    this is python"
+#print(x.strip())
+
+#replace 
+#x = 'hello world'
+#print(x.replace('h','m'))
+
+#splitting a string
+#x = 'hello world'
+#print(x.split())
+
+#x = 'hello world'
+#y = x.split()
+#print(y[1])
+
+#string concatenation
+#x = "hello"
+#y = "world"
+#z = x +" "+ y
+#print(z)
