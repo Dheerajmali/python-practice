48e52ee04627aaeb6d64ecc6a8cad1917e5974c8
Dheerajmali <114793967+Dheerajmali@users.noreply.github.com>
2022-10-17 13:17:07 +0530

    python

---
+++ boolean.py
@@ -0,0 +1,21 @@
+#values the are true
+print(bool(1))
+print(bool(100))
+print(bool('true'))
+print(bool('hello'))
+print(bool('world'))
+
+#values the are not true
+print(bool(0))
+print(bool(false))
+print(bool(none))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
+
+
+
+
+
+
