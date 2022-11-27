64bbaadcd80c981f3d0ae6d5c90edb5bfee6860c
Dheerajmali <114793967+Dheerajmali@users.noreply.github.com>
2022-11-27 13:43:53 +0530

    pratcice

---
+++ loops.py
@@ -0,0 +1,31 @@
+# a=33
+# b=28
+# if a==b:
+#     print("dheeraj")
+# elif b>a:
+#     print("bala sri")
+# else:
+#     print("wrong")
+     
+# a=50
+# b=100
+# if a==b:
+#     print("dheeraj")
+# elif b>=a:
+#     print("something")
+# else:
+#     print("true")
+
+a=15
+b=25
+c=45
+if a==b:
+    print("Hello")
+elif a<=c:
+    print("World")
+elif b>c:
+    print("my name is") 
+elif a>c:
+    print("dheeraj")
+else:
+    print(value)
